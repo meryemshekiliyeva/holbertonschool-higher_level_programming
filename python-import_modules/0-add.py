43cbe8077902a58510ac19f46fa6aa261b532f6c
Maryam <meryemshekiliyeva@gmail.com>
2025-01-18 21:54:11 +0400

    Update 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+from add_0 import add
 
 if __name__ == "__main__":
-    from add_0 import add
+    """
 
-a = 1
-b = 2
+    Prints the result of the addition between two numbers
 
-print("{}".format(f"{a} + {b} = {add(a, b)}"))
+    """
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
