327a8847e3e67aa6f8638da968ed493e6d1000d6
Maryam <meryemshekiliyeva@gmail.com>
2025-01-27 12:06:38 +0400

    m

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-
-"""Square Class
-
-This class don't do anything for the moment
-
-"""
+"""Define a class Square."""
 
 
 class Square:
-"""Square class """
+    """Represent a square."""
     pass
