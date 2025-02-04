a62c61be88f6ced9a658513e8ef328d48b869d1e
Maryam <meryemshekiliyeva@gmail.com>
2025-02-04 12:40:48 +0400

    m

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-"""Module that defines the lookup."""
 
 
 def lookup(obj):
---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module."""
+
+
+class MyList(list):
+    """
+    a class MyList that inherits from list
+
+
+    Methods
+    -------
+    print_sorted():
+	Prints thr list
+    """
+    def print_sorted(self):
+	"""Returns a new list"""
+	print(sorted(self))
+	return sorted(self)
