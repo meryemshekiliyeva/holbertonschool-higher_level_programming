db5cc4a297f3929e11951a800d9d3940911fd903
Maryam <meryemshekiliyeva@gmail.com>
2025-02-04 12:01:18 +0400

    m

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module that defines the lookup."""
+
+
+def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object.
+
+    Args:
+        obj: The object
+
+    Returns:
+	list: The list
+    """
+    return dir(obj)
+
